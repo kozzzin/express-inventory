8ea0220057ea2510c10de385dc15be7f733dd95e
Vitalii Kozin <v.kozzzin@gmail.com>
2022-11-20 21:36:22 +0100

    finished without automated tests and refactoring

--- controllers/item_controller.js
+++ controllers/item_controller.js
@@ -4,6 +4,56 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const checkPassword = require('../checkPassword');
 
+const path = require('path');
+
+const multer  = require('multer');
+const { MulterError } = require('multer');
+const { INSPECT_MAX_BYTES } = require('buffer');
+const { update } = require('../models/category');
+const { redirect } = require('express/lib/response');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/img')
+  },
+  filename: function (req, file, cb) {
+    console.log(req.body);
+    const name = 
+     'img-' + Date.now() + 
+        path.extname(file.originalname);
+    console.log(name);
+    return cb(null, name);
+  }
+})
+
+// const upload = multer({ storage: storage })
+
+const upload = multer(
+  { 
+    // dest: 'public/img',
+    fileFilter: function fileFilter (req, file, cb) {    
+      // Allowed ext
+
+      const filetypes = /jpeg|jpg|png|gif/;
+    
+      // Check ext
+      const extname =  filetypes.test(path.extname(file.originalname).toLowerCase());
+      // Check mime
+      const mimetype = filetypes.test(file.mimetype);
+    
+      if(mimetype && extname){
+        return cb(null,true);
+      } else {
+        req.fileValidationError = "Forbidden extension";
+        return cb(null, false, req.fileValidationError);
+      }
+    },
+    storage: storage
+  }).single('file');
+
+
+
+
 function handleError(err) {
   console.log(err);
 }
@@ -46,42 +96,60 @@ exports.createItem = (req,res,next) => {
 };
 
 exports.postCreateItem = (req,res,next) => {
-  console.log(req.body);
-  const passIsGood = checkPassword(req.body.password);
-  if (passIsGood) {
-    Item.create(
-      { 
-        name: req.body.name,
-        description: req.body.description,
-        category: [req.body.category],
-        location: req.body.location,
-        price: req.body.price,
-        img: req.body.img
-      },
-      function (err, new_house) {
-        if (err) {
-          return res.render('item_form', {
-            title: 'Error',
-            item: req.body,
-            post: true,
-            update: false,
-            fail: err,
-            categories: JSON.parse(req.body.categories),
-          });
-        }
+    // RENAMING FILES using originally extension
+    // Updating items. Current Photo: if you want new, upload File. Check the file field before update.
+  
 
-        res.redirect(`/item/${new_house._id}`);
+    upload(req, res, function(err) {
+      if (req.fileValidationError) {
+        return res.render('item_form', {
+          title: 'Error',
+          item: req.body,
+          post: true,
+          update: false,
+          fail: 'wrong file format!',
+          categories: JSON.parse(req.body.categories),
+        });
+      }
+
+      const passIsGood = checkPassword(req.body.password);
+      if (passIsGood) {
+        Item.create(
+          { 
+            name: req.body.name,
+            description: req.body.description,
+            category: [req.body.category],
+            location: req.body.location,
+            price: req.body.price,
+            img: req.file.path.match(/(\/img\/.*)/gm)[0],
+          },
+          function (err, new_house) {
+            if (err) {
+              return res.render('item_form', {
+                title: 'Error',
+                item: req.body,
+                post: true,
+                update: false,
+                fail: err,
+                categories: JSON.parse(req.body.categories),
+              });
+            }
+    
+            res.redirect(`/item/${new_house._id}`);
+        });
+      } else {
+        res.render('item_form', {
+          title: 'Error',
+          item: req.body,
+          post: true,
+          update: false,
+          fail: 'Wrong password',
+          categories: JSON.parse(req.body.categories),
+        });
+      }
+      // Everything went fine.
     });
-  } else {
-    res.render('item_form', {
-      title: 'Error',
-      item: req.body,
-      post: true,
-      update: false,
-      fail: 'Wrong password',
-      categories: JSON.parse(req.body.categories),
-    });
-  }
+
 };
 
 exports.updateItem = (req,res,next) => {
@@ -105,43 +173,69 @@ exports.updateItem = (req,res,next) => {
 };
 
 exports.postUpdateItem = (req,res,next) => {
-  const passIsGood = checkPassword(req.body.password);
-  if (passIsGood) {
-    Item.findByIdAndUpdate(
-      req.body._id,
-      {
-        name: req.body.name,
-        description: req.body.description,
-        category: [req.body.category],
-        location: req.body.location,
-        price: req.body.price,
-        img: req.body.img,
-      },
-      function(err, result) {
-        if (err) {
-          console.log(err);
-          return res.render('item_form', {
-            title: false,
-            post: true,
-            item: req.body,
-            update: true,
-            categories: JSON.parse(req.body.categories),
-            fail: true,
-          });
+  upload(req, res, function(err) {
+    if (req.fileValidationError) {
+      return res.render('item_form', {
+        title: 'Error',
+        item: req.body,
+        post: true,
+        update: true,
+        fail: req.fileValidationError,
+        categories: JSON.parse(req.body.categories),
+      });
+    };
+
+    const img = !!req.file ?
+      req.file.path.match(/(\/img\/.*)/gm)[0] :
+        req.body.img; 
+ 
+    const passIsGood = checkPassword(req.body.password);
+    if (passIsGood) {
+      Item.findByIdAndUpdate(
+        req.body._id,
+        {
+          name: req.body.name,
+          description: req.body.description,
+          category: [req.body.category],
+          location: req.body.location,
+          price: req.body.price,
+          img: img,
+        },
+        function(err, result) {
+          if (err) {
+            console.log(err);
+            return res.render('item_form', {
+              title: false,
+              post: true,
+              item: req.body,
+              update: true,
+              categories: JSON.parse(req.body.categories),
+              fail: true,
+            });
+          }
+          return res.redirect(`/item/${req.body._id}`);
         }
-        return res.redirect(`/item/${req.body._id}`);
-      }
-    );
-  } else {
-    res.render('item_form', {
-      title: false,
-      item: req.body,
-      post: true,
-      update: true,
-      categories: JSON.parse(req.body.categories),
-      fail: 'wrong password',
-    });
-  };
+      );
+    } else {
+      res.render('item_form', {
+        title: false,
+        item: req.body,
+        post: true,
+        update: true,
+        categories: JSON.parse(req.body.categories),
+        fail: 'wrong password',
+      });
+    };
+
+  });
+
+
+
+
+
+
+
+
 };
 
 exports.deleteItem = (req,res,next) => {
--- routes/item.js
+++ routes/item.js
@@ -2,16 +2,23 @@ const express = require('express');
 const itemController = require('../controllers/item_controller');
 const router = express.Router();
 
+
+// !!! add category name to image 
+// check what file is being used
+
 router.get('/',(req,res) => {
   res.redirect('/');
 });
 
 router.get('/create',itemController.createItem);
 router.get('/create/:id',itemController.postCreateItem);
-router.post('/create',itemController.postCreateItem);
+router.post(
+  '/create',
+  itemController.postCreateItem
+);
 router.get('/:id',itemController.getItem);
 router.get('/:id/update',itemController.updateItem);
-router.post('/:id/update',itemController.postUpdateItem);
+router.post('/:id/update', itemController.postUpdateItem);
 router.get('/:id/delete',itemController.deleteItem);
 router.post('/:id/delete',itemController.postDeleteItem);
 
